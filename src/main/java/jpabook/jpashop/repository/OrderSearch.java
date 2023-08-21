aff549ed7c4b6e5aa2bbbbc2d2d3dc33e3b5df93
user <cjlee6009@gmail.com>
2023-08-21 16:16:10 +0900

    클래스 수정
    MemberApiController 생성

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -3,12 +3,10 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter @Setter
 public class OrderSearch {
 
-    private String memberName;
-    private OrderStatus orderStatus;
-
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; //주문 상태[ORDER, CANCEL]
 }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop.service;
 
-import jakarta.transaction.Transactional;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -44,6 +44,10 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
-
+    @Transactional
+    public void update(Long id, String name){
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 
 }
